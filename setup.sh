b7adb828c0c96f25e715d14ab0ba459e6c8a75c0
Daniel Einars <contact@dle.dev>
2020-01-06 15:53:18 +0100

    fix: updated docker and docker-compose install scripts

--- setup.sh
+++ setup.sh
@@ -150,13 +150,36 @@ sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
 sudo fc-cache -f -v
 
 
-# docker
-sudo sh -c "$(curl -fsSL https://get.docker.com -o get-docker.sh)"
-sudo usermod -aG docker $USER
+# Docker
+sudo apt remove --yes docker docker-engine docker.io \
+    && sudo apt update \
+    && sudo apt --yes --no-install-recommends install \
+        apt-transport-https \
+        ca-certificates \
+    && wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg \
+        | sudo apt-key add - \
+    && sudo add-apt-repository \
+        "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu \
+        $(lsb_release --codename --short) \
+        stable" \
+    && sudo apt update \
+    && sudo apt --yes --no-install-recommends install docker-ce \
+    && sudo usermod --append --groups docker "$USER" \
+    && sudo systemctl enable docker \
+    && printf '\nDocker installed successfully\n\n'
 
-# docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+printf 'Waiting for Docker to start...\n\n'
+sleep 3
+
+# Docker Compose
+sudo wget \
+        --output-document=/usr/local/bin/docker-compose \
+        https://github.com/docker/compose/releases/download/1.25.0/run.sh \
+    && sudo chmod +x /usr/local/bin/docker-compose \
+    && sudo wget \
+        --output-document=/etc/bash_completion.d/docker-compose \
+        "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose" \
+    && printf '\nDocker Compose installed successfully\n\n'
 
 # Network-Manager
 cd $WORK_PATH
