31f64159e0ff069a59f1a07583e92753776716c6
Daniel Einars <contact@dle.dev>
2020-03-20 15:12:53 +0100

    fix: added GTK_IM_MODULE='xim' to `~/.zshrc`
      * Without this, vs code will run into the error where `ctrl+shift+e` will propmpte the emoji support

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     rules: {
         "body-leading-blank": [2, "always"],
-        "body-max-line-length": [2, "always", 72],
         "footer-leading-blank": [2, "always"],
         "header-max-length": [2, "always", 72],
         "subject-empty": [2, "never"],
