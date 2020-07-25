735a45e53a1bcfdd9f6adfe17b297aeea5f05214
Daniel Einars <contact@dle.dev>
2020-07-25 10:46:34 +0200

    fixed i3 build errors

--- setup.sh
+++ setup.sh
@@ -35,9 +35,10 @@ sudo make install
 
 
 # i3
-sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+sudo apt install -y libxcb1-dev libxcb-m -rfkeysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
 
 cd $WORK_PATH/i3
+git checkout gaps-next
 autoreconf --force --install
 rm -rf build
 mkdir build
