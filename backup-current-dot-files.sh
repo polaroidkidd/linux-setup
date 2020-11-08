d6a5a55b9b0e4abfefaf1243be2f662b275d2551
Daniel Einars <contact@dle.dev>
2020-11-08 18:50:16 +0100

    updated compton location

--- backup-current-dot-files.sh
+++ backup-current-dot-files.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-
+set -e
+set -x
 # get the path to this script
-MY_PATH=`dirname "$0"`
-MY_PATH=`( cd "$MY_PATH" && pwd )`
+WORK_PATH=`dirname "$0"`
+WORK_PATH=`( cd "$WORK_PATH" && pwd )`
 
 # Define dot-file dir
-DOTFILE_PATH=$MY_PATH/dot-files
+DOTFILE_PATH=$WORK_PATH/dot-files
 
 echo $DOTFILE_PATH
 
+cd $WORK_PATH
+sudo chown -R $USER $DOTFILE_PATH
 # Delete Previous dot-files
 rm -rf $DOTFILE_PATH
 mkdir $DOTFILE_PATH
@@ -18,15 +21,24 @@ mkdir -p $DOTFILE_PATH/fusuma
 mkdir -p $DOTFILE_PATH/polybar
 mkdir -p $DOTFILE_PATH/icons
 mkdir -p $DOTFILE_PATH/gnome-shell
+mkdir -p $DOTFILE_PATH/themes
+mkdir -p $DOTFILE_PATH/gtk
+mkdir -p $DOTFILE_PATH/compton
 
 cp ~/.config/i3/config $DOTFILE_PATH/i3/
 cp -r ~/.config/rofi $DOTFILE_PATH/
 cp ~/.config/fusuma/config.yml $DOTFILE_PATH/fusuma/
-cp ~/.config/compton.conf $DOTFILE_PATH
+cp ~/.config/compton.conf $DOTFILE_PATH/compton/
 cp -r ~/.config/copyq $DOTFILE_PATH/
 cp -r ~/.config/polybar $DOTFILE_PATH/
 cp -r ~/.config/gnome-shell $DOTFILE_PATH/
-
+cp -r ~/.themes $DOTFILE_PATH/
+cp ~/.gtkrc-3.0 $DOTFILE_PATH/gtk
+cp ~/.gtkrc-2.0 $DOTFILE_PATH/gtk
+cp -r ~/.config/gtk-2.0 $DOTFILE_PATH/gtk
+cp -r ~/.config/gtk-3.0 $DOTFILE_PATH/gtk
+cp -r ~/.config/gtk-2.0 $DOTFILE_PATH/gtk
+cp -r ~/.config/gtk-assets $DOTFILE_PATH/gtk
 
 # custom desktop entries
 rm -rf desktop-entries
@@ -50,3 +62,6 @@ mkdir -p ./git-config
 cp ~/.gitconfig ./git-config/
 cp ~/.gitconfig-me ./git-config/
 cp ~/.gitconfig-ti8m ./git-config/
+
+cd $WORK_PATH
+sudo chown -R $USER $DOTFILE_PATH
